f8570aa9d77589276e7e0bd9ffb5fdccc8d0b5fc
Sergey Moshnikov <s.moshnikov@gmail.com>
2023-05-17 18:52:57 +0300

    First sample route

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,27 @@
-fun main(args: Array<String>) {
+import org.http4k.core.*
+import org.http4k.routing.bind
+import org.http4k.routing.path
+import org.http4k.routing.routes
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+
+fun main() {
+    val app = routes(
+        "/todo/{user}/{list}" bind Method.GET to ::showList
+    )
+    app.asServer(Jetty(8080)).start()
+}
+
+fun showList(req: Request): Response {
+    val user = req.path("user")
+    val list = req.path("list")
+    val htmlPage = """
+    <html>
+        <body>
+            <h1>Zettai</h1>
+            <p>Here is a list <b>$list</b> of user <b>$user</b></p>
+        </body>
+    </html>    
+    """.trimIndent()
+    return Response(Status.OK).body(htmlPage)
 }
