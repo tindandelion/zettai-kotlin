60484739142ae61529dc44c8a3aaf7bf9ce41a84
Sergey Moshnikov <s.moshnikov@gmail.com>
2023-05-18 13:51:37 +0300

    Added a simple home page

--- src/main/kotlin/Zettai.kt
+++ src/main/kotlin/Zettai.kt
@@ -13,10 +13,25 @@ private data class HtmlPage(val raw: String)
 
 class Zettai(private val lists: Map<User, List<ToDoList>>) : HttpHandler {
     val routes = routes(
+        "/" bind Method.GET to ::showHomepage,
         "/todo/{user}/{list}" bind Method.GET to ::showList
     )
 
+
     override fun invoke(req: Request): Response = routes(req)
+
+    @Suppress("UNUSED_PARAMETER")
+    private fun showHomepage(req: Request): Response {
+        val homePage = """
+        <html>
+            <body>
+                <h1>Zettai</h1>
+                <h2>Home page</h2>
+            </body>
+        </html>
+        """.trimIndent()
+        return Response(Status.OK).body(homePage)
+    }
 
     private fun showList(req: Request): Response =
         req.let(::extractListData)
